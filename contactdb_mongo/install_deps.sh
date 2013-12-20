0846bfb9d7b3056c32616122ffc74ca0e6f213fc
Raphaël Vinot <raphael@vinot.info>
2013-12-20 17:30:17 +0100

    some tries with js, failing

--- contactdb_mongo/install_deps.sh
+++ contactdb_mongo/install_deps.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-BOOTSTRAP="3.0.2"
+BOOTSTRAP="3.0.3"
 
 wget https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP}/bootstrap-${BOOTSTRAP}-dist.zip \
     -O bootstrap.zip
@@ -12,3 +12,6 @@ unzip -o -d contactdb/ bootstrap.zip
 rsync -av contactdb/dist/* contactdb/static/
 rm -rf contactdb/dist
 rm bootstrap.zip
+
+JQUERY="1.10.2"
+wget http://code.jquery.com/jquery-${JQUERY}.min.js -O contactdb/static/js/jquery.js
